16156a054431298f1d9e1ad096716f150aa53b38
Niv Suroghon <nsuroghon@gmail.com>
2021-03-25 19:53:50 -0400

    pages to hold highscores and post quiz score

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -48,7 +48,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign('./highschore.html')
     }
 
     questionCounter++
