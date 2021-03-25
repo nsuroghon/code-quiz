8b239cd88f6b203225af08b702999f98acf2e6de
Niv Suroghon <nsuroghon@gmail.com>
2021-03-25 15:22:25 -0400

    questions array

---
+++ assets/js/quiz.js
@@ -0,0 +1,26 @@
+let questions = [
+{
+    question: 'Commonly used javascript data types DO NOT include...',
+    choice1: 'strings',
+    choice2: 'booleans',
+    choice3: 'alerts',
+    choice4: 'numbers',
+    answer: 3
+},
+{
+    question: 'The condition in an if/else statement is enclosed within...',
+    choice1: 'curly brackets',
+    choice2: 'quotes',
+    choice3: 'paranthesis',
+    choice4: 'square brackets',
+    answer: 3
+},
+{   
+    question: 'arrays in Javascript can be used to store',
+    choice1: 'numbers & strings',
+    choice2: 'booleans',
+    choice3: 'other arrays',
+    choice4: 'all of the above',
+    answer: 4
+}
+];
