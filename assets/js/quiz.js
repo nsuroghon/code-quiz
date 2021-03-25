b1f3a5ae692a96ce382ea2f03ce2d25aa71d7aa8
Niv Suroghon <nsuroghon@gmail.com>
2021-03-25 19:32:20 -0400

    leaderboards page

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,13 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const scoreText = document.querySelector('#score');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
 let questions = [
 {
     question: 'Commonly used javascript data types DO NOT include...',
@@ -25,15 +35,6 @@ let questions = [
 }
 ];
 
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const scoreText = document.querySelector('#score');
-
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
 
 function startGame () {
     questionCounter = 0;
