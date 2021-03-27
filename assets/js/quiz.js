a09908a8a9c86ec56bbc600e2279d9161e79f6b8
Niv Suroghon <nsuroghon@gmail.com>
2021-03-27 14:01:27 -0400

    bug fixed

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,111 +3,95 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
-
-
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
-
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 let questions = [
-{
+  {
     question: 'What is 2 + 2?',
     choice1: '2',
     choice2: '4',
     choice3: '21',
     choice4: '17',
     answer: 2,
-},
-{
+  },
+  {
     question: 'Commonly used javascript data types DO NOT include...',
     choice1: 'strings',
     choice2: 'booleans',
     choice3: 'alerts',
     choice4: 'numbers',
-    answer: 3
-},
-{
+    answer: 3,
+  },
+  {
     question: 'The condition in an if/else statement is enclosed within...',
     choice1: 'curly brackets',
     choice2: 'quotes',
     choice3: 'paranthesis',
     choice4: 'square brackets',
-    answer: 3
-},
-{   
+    answer: 3,
+  },
+  {
     question: 'arrays in Javascript can be used to store',
     choice1: 'numbers & strings',
     choice2: 'booleans',
     choice3: 'other arrays',
     choice4: 'all of the above',
-    answer: 4
-}
+    answer: 4,
+  },
 ];
-
-// const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
-
-function startGame () {
-    questionCounter = 0;
-    score = 0;
-    availableQuesions = [...questions];
-    getNewQuestion();
-};
-
-getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
-
-        return window.location.assign('../pages/endquiz.html')
-    }
-
-    questionCounter++
-    // progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    // progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-    
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
-
-    choices.forEach(choice => {
-        var number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
-
-    availableQuestions.splice(questionsIndex, 1)
-
-    acceptingAnswers = true
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
+function startGame() {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
 }
-
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
-
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
-
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-
-        if(classToApply === 'correct') {
-            incrementScore(SCORE_POINTS)
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply)
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-
-        }, 1000)
-    })
-})
-
-// incrementScore = num => {
-//     score +=num
-//     scoreText.innerText = score
-// }
-
-startGame()
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem('mostRecentScore', score);
+    // return window.location.assign('../pages/endquiz.html');
+  }
+  availableQuestions = questions; //TODO
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionsIndex];
+  if (currentQuestion) {
+    question.innerText = currentQuestion.question;
+    choices.forEach((choice) => {
+      var number = choice.dataset['number'];
+      choice.innerText = currentQuestion['choice' + number];
+    });
+    availableQuestions.splice(questionsIndex, 1);
+    acceptingAnswers = true;
+  } else {
+    return window.location.assign('../pages/endquiz.html');
+  }
+};
+choices.forEach((choice) => {
+  choice.addEventListener('click', (e) => {
+    if (!acceptingAnswers) return;
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset['number'];
+    let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+    if (classToApply === 'correct') {
+      incrementScore(SCORE_POINTS);
+    }
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
+startGame();
